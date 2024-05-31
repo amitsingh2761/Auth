6b8401fc99dac20ba3209d6734d7e79eb4406efc
unknown <singhamit2761@gmail.com>
2024-05-31 12:16:27 +0530

    adding files

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div className="container bg-gray flex justify-center content-center flex-col">
+      <h1>Login</h1>
+      <div className="container container-xl bg-red-300"></div>
+    </div>
+  );
+}
